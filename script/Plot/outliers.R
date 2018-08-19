4fd373ce9e96dc47adab2f1e4ce07f03486f5176
lilei <llei@umn.edu>
2018-08-19 12:46:58 -0500

    add outliers

--- script/Plot/outliers.R
+++ script/Plot/outliers.R
@@ -5,26 +5,26 @@ library(ggplot2)
 
 #   Read the exon capture density data,9k genptyping data, and exon-capture SNP data;
 
-Elv <- read.delim(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/Elevation/Outliers/Fst_less3000_vsmore3000_outliers_physPos.txt",header = T, sep="\t")
+Elv <- read.delim(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/Elevation/Outliers/Fst_less3000_vsmore3000_outliers_99th_sorted_physPos.txt",header = T, sep="\t")
 Elv $Chromosome <- as.character(Elv$Chr_2016)
 Elv <- na.omit(Elv)
 head(Elv)
 nrow(Elv)
 
-Lat_30_40_above <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/Latitude/Outliers/Fst_wild_range30_40_vs_higherLat40_outliers_physPos.txt", header=T)
+Lat_30_40_above <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/Latitude/Outliers/Fst_wild_range30_40_vs_higherLat40_outliers_99th_sorted_physPos.txt", header=T)
 Lat_30_40_above$Chromosome <- as.character(Lat_30_40_above$Chr_2016)
 Lat_30_40_above <- na.omit(Lat_30_40_above)
 head(Lat_30_40_above)
 str(Lat_30_40_above)
 nrow(Lat_30_40_above)
 
-Lat_30_40_less <- read.delim(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/Latitude/Outliers/Fst_moreoreq30to40vsless30_outliers_physPos.txt",header = T, sep="\t")
+Lat_30_40_less <- read.delim(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/Latitude/Outliers/Fst_moreoreq30to40vsless30_no_NA_outliers_99th_sorted_physPos.txt",header = T, sep="\t")
 Lat_30_40_less$Chromosome <- as.character(Lat_30_40_less$Chr_2016)
 Lat_30_40_less <- na.omit(Lat_30_40_less)
 head(Lat_30_40_less)
 nrow(Lat_30_40_less)
 
-habit <- read.delim(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/GrowthHabit/Outliers/outlier_Fst_SPRING_vs_WINTER_80samples_average_PhyPosnoNA.txt",header = T, sep="\t")
+habit <- read.delim(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/Fst/Results/GrowthHabit/Outliers/outlier_Fst_SPRING_vs_WINTER_80samples_average_noNA_PhyPos_99th.txt",header = T, sep="\t")
 habit$Chromosome <- as.character(habit$Chr_2016)
 habit <- na.omit(habit)
 head(habit)
@@ -63,19 +63,55 @@ GWAS <- GWAS[GWAS$Chromosome != "chrUn",]
 head(GWAS)
 #exonSNP$Position <- as.numeric(exonSNP$Position)
 
+###read the genes
 
+HEout <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/High_elevation.txt",header=T,sep="\t")
+head(HEout)
+str(HEout)
+
+HLout <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/High_latitude.txt",header=T,sep="\t")
+head(HLout)
+str(HLout)
+
+LLout <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/Low_latitude.txt",header=T,sep="\t")
+head(LLout)
+str(LLout)
+
+GHout <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/Growth_habit.txt",header=T,sep="\t")
+head(GHout)
+
+str(GHout)
+
+geosig <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/geo_ass.txt",header=T,sep="\t")
+head(geosig)
+str(geosig)
+
+tempsig <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/temp_ass.txt",header=T,sep="\t")
+head(tempsig)
+str(tempsig)
+
+humsig <- read.delim("/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/hum_ass.txt",header=T,sep="\t")
+head(humsig)
+str(humsig)
 
 ##Adjust a little! 
 
-pdf(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/outlier.pdf", 15, 9)
+pdf(file="/Users/lilei/Dropbox (Morrell Lab)/Landrace_Environmental_Assocation/Analyses/outliers/outlier_99th.pdf", 10, 6)
 ggplot(Elv) +
-  geom_point(aes(x=PhysPos_2016/1000000, y=5), data=Elv, shape=21,fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5)+
-  geom_point(aes(x=PhysPos_2016/1000000, y=10), data=Lat_30_40_above, shape=22,fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5) +
-  geom_point(aes(x=PhysPos_2016/1000000, y=15), data=Lat_30_40_less, shape=23,fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5) +
-  geom_point(aes(x=PhysPos_2016/1000000, y=20), data=habit, shape=24,fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5) +
-  geom_point(aes(x=PhysPos_2016/1000000, y=25), data=preci, shape=25, fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5) +
-  geom_point(aes(x=PhysPos_2016/1000000, y=30), data=temp, shape=7, fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5) +
-  geom_point(aes(x=PhysPos_2016/1000000, y=35), data=geo, shape=9, fill="#ff0000", color="#ff0000", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=5), data=Elv, shape=21,fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5)+
+  geom_point(aes(x=PhysPos_2016/1000000, y=10), data=Lat_30_40_above, shape=22,fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=15), data=Lat_30_40_less, shape=23,fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=20), data=habit, shape=10,fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=25), data=preci, shape=9, fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=30), data=temp, shape=13, fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=35), data=geo, shape=11, fill="#0000FF", color="#0000FF", size=1.5, alpha=0.5) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=5), data=HEout, shape=21, fill="#17202A", color="#17202A", size=1.5, alpha=1) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=10), data=HLout, shape=22,fill="#17202A", color="#17202A", size=1.5, alpha=1) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=15), data=LLout, shape=23,fill="#17202A", color="#17202A", size=1.5, alpha=1) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=20), data=GHout, shape=10,fill="#17202A", color="#17202A", size=1.5, alpha=1) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=25), data=humsig, shape=9, fill="#17202A", color="#17202A", size=1.5, alpha=1) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=30), data=tempsig, shape=13, fill="#17202A", color="#17202A", size=1.5, alpha=1) +
+  geom_point(aes(x=PhysPos_2016/1000000, y=35), data=geosig, shape=11, fill="#17202A", color="#17202A", size=1.5, alpha=1) +
   facet_grid(Chromosome~.) +
   scale_y_continuous(limits=c(0, 40), breaks=seq(0,40,by=5)) +
   scale_x_continuous(limits=c(0, 725), breaks=seq(0, 725, by=50)) +
